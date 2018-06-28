0d313640ab1b450061d0caa995f58dd2390b9818
CorySolovewicz <Cory@Solovewicz.com>
2018-06-28 01:07:35 -0700

    Removed .eslintcache, updated unit test on App component, and updated .gitignore to avoid commiting test coverage cache and .eslintcache in future commits.

--- __tests__/components/App-test.js
+++ __tests__/components/App-test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import App from '../../src/js/App';
+import App from '../../src/js/components/App';
 
 // needed because this:
 // https://github.com/facebook/jest/issues/1353
